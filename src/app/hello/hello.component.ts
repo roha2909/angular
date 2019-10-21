c76d6405b46415a1c1d94308e3adfe4b918ede0c
Rohini Ahirrao <ahirrao.rohini.29@gmail.com>
2019-10-21 12:30:43 -0700

    commit

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  sayHello()
+  {
+    return "hello world!!"
+  }
+
+}
